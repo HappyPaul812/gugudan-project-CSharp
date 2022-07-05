eaf4e8a2f5ac1ffedeaa908a3af86bc5644b4576
SeokMu Jeong <paul812sm@gmail.com>
2022-07-05 15:37:50 +0900

    final version

---
+++ GugudanQuiz/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace GugudanQuiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.tbxUserAnswer = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblAnswerCounter = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTitle.Location = new System.Drawing.Point(95, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(191, 32);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "구구단 퀴즈";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTimer.Location = new System.Drawing.Point(149, 109);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(82, 24);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "타이머";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblQuestion.Location = new System.Drawing.Point(110, 219);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(82, 21);
+            this.lblQuestion.TabIndex = 3;
+            this.lblQuestion.Text = "? x ? = ";
+            // 
+            // tbxUserAnswer
+            // 
+            this.tbxUserAnswer.Location = new System.Drawing.Point(209, 219);
+            this.tbxUserAnswer.Name = "tbxUserAnswer";
+            this.tbxUserAnswer.Size = new System.Drawing.Size(50, 21);
+            this.tbxUserAnswer.TabIndex = 4;
+            this.tbxUserAnswer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxUserAnswer_KeyPress);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblAnswerCounter
+            // 
+            this.lblAnswerCounter.AutoSize = true;
+            this.lblAnswerCounter.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblAnswerCounter.Location = new System.Drawing.Point(134, 168);
+            this.lblAnswerCounter.Name = "lblAnswerCounter";
+            this.lblAnswerCounter.Size = new System.Drawing.Size(112, 21);
+            this.lblAnswerCounter.TabIndex = 5;
+            this.lblAnswerCounter.Text = "맞힌 수 : 0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "답";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblName.Location = new System.Drawing.Point(126, 66);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(63, 20);
+            this.lblName.TabIndex = 7;
+            this.lblName.Text = "이름 :";
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(188, 68);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(64, 21);
+            this.tbxName.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(153, 326);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "순위 보기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblAnswerCounter);
+            this.Controls.Add(this.tbxUserAnswer);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GugudanQuiz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox tbxUserAnswer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblAnswerCounter;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ GugudanQuiz/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace GugudanQuiz
+{
+    public partial class Form1 : Form
+    {
+
+        int min = 2, sec = 60, answer = 0, answerCounter = 0, userAnswer = 0;
+        bool timeOver = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbxUserAnswer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                button2_Click(sender, e);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            confirm_answer();
+        }
+
+        /* public void time()
+         {
+             //int i = 0, j = 0;
+             //timeOver = false;
+             Object obj = new Object();
+             Monitor.Enter(obj);
+             for (int i = 2; i >= 0; i--)
+             {
+                 for (int j = 59; j >= 0; j--)
+                 {
+                     try
+                     {
+                         this.lblTimer.Invoke(new Action(delegate ()
+                         {
+                             lblTimer.Text = $"{i}분 {j}초";
+                         }));
+                         Thread.Sleep(1000);
+                     } catch(Exception e) {
+                         MessageBox.Show(e.Message); 
+                     }
+
+                 }
+             }
+             Monitor.Exit(obj);
+         }*/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Thread timer = new Thread(new ThreadStart(time));
+            bool restart = false;
+            if (!restart)
+            {
+                timer1.Start();
+                questions();
+                restart = true;
+                button1.Text = "다시 시작";
+                tbxUserAnswer.Focus();
+            } else
+            {
+                timer1.Stop();
+                answerCounter = 0;
+                answer = 0;
+                lblAnswerCounter.Text = $"맞힌 수 : {answerCounter}";
+                timer1.Start();
+                questions();
+                tbxUserAnswer.Focus();
+            }
+            //timer.Join();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+
+        /*private void tbxUserAnswer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                //confirm_answer();
+                do
+                {
+                    userAnswer = int.Parse(tbxUserAnswer.Text);
+                    if (answer == userAnswer)
+                    {
+                        answerCounter += 1;
+                        lblAnswerCounter.Text = $"맞힌 수 : {answerCounter}";
+                        questions();
+                    }
+                } while (!timeOver) ;
+            }
+        }*/
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (min != 0 && sec == 0)
+            {
+                min--;
+                sec = 60;
+            }
+            else if (min == 0 && sec == 0)
+            {
+                lblTimer.Text = "시간 초과!!!";
+                saveDB();
+                timeOver = true;
+                timer1.Enabled = false;
+                button1.Text = "시작";
+            }
+            else
+            {
+                sec--;
+                lblTimer.Text = $"{min}분 {sec}초";
+            }
+
+            
+            /*for (int min = 2; min >= 0; min--)
+            {
+                for (int sec = 59; sec >= 0; sec--)
+                {
+                    lblTimer.Text = $"{min}분 {sec}초";
+                    try {
+                        Thread.Sleep(1000);
+                    }
+                    catch (Exception ee)
+                    {
+                        MessageBox.Show(ee.Message);
+                    }
+                }
+            }*/
+        }
+
+        private void saveDB()
+        {
+            WebClient wb = new WebClient();
+            NameValueCollection data = new NameValueCollection();
+
+            string url = "http://paul81web.000webhostapp.com/php/gugudan_insert.php";
+
+            data["name"] =  tbxName.Text;
+            data["score"] = answerCounter.ToString();
+
+            try
+            {
+                byte[] response = wb.UploadValues(url, data);
+                //MessageBox.Show(response.ToString());
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void questions()
+        {
+            Random rand = new Random();
+            int value1 = 0, value2 = 0;
+            value1 = rand.Next(2, 10);
+            value2 = rand.Next(2, 10);
+            answer = value1 * value2;
+            lblQuestion.Text = $"{value1} X {value2} =";
+        }
+
+        private void confirm_answer()
+        {
+            string strAnswer = null; 
+            //bool bl = false;
+            int returnVal = 0;
+            try
+            {
+                if (!int.TryParse(strAnswer, out returnVal))
+                {
+                    do
+                    {
+                        strAnswer = tbxUserAnswer.Text;
+                        userAnswer = Int32.Parse(strAnswer); //throw new Exception();
+                        tbxUserAnswer.Text = "";
+                        if (answer == userAnswer)
+                        {
+                            answerCounter += 1;
+                            lblAnswerCounter.Text = $"맞힌 수 : {answerCounter}";
+                            questions();
+                            tbxUserAnswer.Focus();
+                        }
+                    } while (!timeOver);
+                } else if (String.IsNullOrEmpty(tbxUserAnswer.Text))
+                {
+                    MessageBox.Show("답을 쓰세요.");
+                    //throw new Exception(tbxUserAnswer.Text);
+                    tbxUserAnswer.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+                //throw new FormatException();//Exception(tbxUserAnswer.Text);
+            }
+        }
+    }
+}
